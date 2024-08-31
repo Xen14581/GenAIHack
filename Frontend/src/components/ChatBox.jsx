2eef1683154c5017075d6996b61c0ca8da3a6e9f
Taha-001 <tahalokat72@gmail.com>
2024-09-01 00:11:28 +0530

    Chat page with new button and logo

--- Frontend/src/components/ChatBox.jsx
+++ Frontend/src/components/ChatBox.jsx
@@ -1,23 +1,94 @@
 import React from "react";
-import { TextField, Button, Box, Paper,Stack } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Box,
+  Paper,
+  Stack,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
 import Message from "./Message";
 import Loader from "./Loader";
+import { useState } from "react";
+import { ReactComponent as Logo } from "../assets/Logo-rev.svg";
+import arrow from "../assets/right-arrow.png";
 
-const ChatBox=()=>{
-    return(
-        <Box sx={{display: 'flex', flexDirection: 'column', width: '100%', pl: 2, pr: 2 }}>
-            <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', height: '100%', flexDirection: 'column'}} >
-                <Message role={'assistant'} message={"Hello World"} />
-            </Box>
-            <Box sx={{marginTop:"auto",marginBottom:"5vh",width:'100%'}}>
-                <Stack spacing={2} direction="row">
-                    <TextField label="Message SAGE.AI" sx={{width:"85%"}}/>
-                    <Button variant="outlined">Send</Button>
-                    <Button variant="contained" disabled>Evaluate</Button>
-                </Stack>
-            </Box>
+const ChatBox = () => {
+  const [chats, setChats] = useState(["Hello I am sage. Ask me anything"]);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    setChats(() => [...chats, value]);
+  };
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    //main Box
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        pl: 2,
+        pr: 2,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+          overflowY: "auto",
+          justifyContent: "flex-end",
+          py: 2,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            position: "fixed",
+            width: "65%",
+            pb: 20,
+          }}
+        >
+          <Logo width="25%" height="25%" opacity="0.045" />
         </Box>
-    )
-}
+        {chats.map((msg, index) => (
+          <Message role={index % 2 == 0 ? "assistant" : "user"} message={msg} />
+        ))}
+      </Box>
+      <Box sx={{ marginTop: "auto", marginBottom: "5vh", width: "100%" }}>
+        <Stack spacing={2} direction="row">
+          <TextField
+            label="Message SAGE.AI"
+            sx={{ width: "100%" }}
+            value={value}
+            onChange={handleInputChange}
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton>
+                      <img
+                        src={arrow}
+                        style={{ width: 30, height: 30 }}
+                        onClick={handleSubmit}
+                      />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+        </Stack>
+      </Box>
+    </Box>
+  );
+};
 
 export default ChatBox;
--- Frontend/src/components/Message.jsx
+++ Frontend/src/components/Message.jsx
@@ -1,19 +1,37 @@
 import React from "react";
-import { useState,useEffect } from "react";
-import {Box,Typography} from  "@mui/material";
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
 
-const Message = ({role, message}) =>{
-    return(
-        <>
-        <Box sx={{width:"100%", display: 'flex', justifyContent: role === 'assistant' ? 'start' : 'end'}}>
-            <Box sx={{color:role==="assistant"?"#DEEFFF":"#006CB8"}}>
-                <Typography variant="h2">
-                    {message}
-                </Typography> 
-            </Box>
-        </Box>
-        </>
-    )
-}
+const Message = ({ role, message }) => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: role === "assistant" ? "flex-start" : "flex-end",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          backgroundColor: role === "assistant" ? "#DEEFFF" : "#006CB8",
+          border:
+            role === "assistant" ? "1px solid #006CB8" : "1px solid #006CB8",
+          borderRadius: "10px",
+          pr: 2,
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{ color: role === "assistant" ? "black" : "white", pl: 2 }}
+        >
+          {message}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 
 export default Message;
